ffad7890ceac5cae43860d913d3a51361aecc5ee
Ramon Maturano <jose.ramos@MacBookPro.local>
2014-03-24 22:27:25 -0700

    First commit... ever cheers

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Photoq::Application.config.secret_key_base = '30ebeb49f9008f27c57566662301f5cfd7cb25e55c886c00cf33d373d092790682978ff2832fdd888c3fe844bdb61b65686651f6a05d0d209231bcaf4a2974c8'
